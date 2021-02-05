d7241b0c9df037e7f016ecee52d1d660c3c87013
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-02-05 12:44:36 +0100

    updated changelog and version number

--- worldstar-pro.php
+++ worldstar-pro.php
@@ -5,14 +5,14 @@ Plugin URI: http://themezee.com/addons/worldstar-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the WorldStar theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.5.1
+Version: 1.5.2
 Text Domain: worldstar-pro
 Domain Path: /languages/
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 WorldStar Pro
-Copyright(C) 2016, ThemeZee.com - support@themezee.com
+Copyright(C) 2021, ThemeZee.com - support@themezee.com
 
 */
 
@@ -63,7 +63,7 @@ class WorldStar_Pro {
 		define( 'WORLDSTAR_PRO_NAME', 'WorldStar Pro' );
 
 		// Define Version Number.
-		define( 'WORLDSTAR_PRO_VERSION', '1.5.1' );
+		define( 'WORLDSTAR_PRO_VERSION', '1.5.2' );
 
 		// Define Plugin Name.
 		define( 'WORLDSTAR_PRO_PRODUCT_ID', 81120 );
